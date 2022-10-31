78c48146432c9874ce7491f2532b335ea65e6276
nerzul88 <nerzul.88@gmail.com>
2022-10-31 19:13:38 +0300

    Initial Commit

---
+++ GOTApp/GOTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOTAppApp.swift
+//  GOTApp
+//
+//  Created by Александр Касьянов on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GOTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
